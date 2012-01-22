54dca7a0d71ddaeafcf50015827ccd2489310c7e
Jeffrey Warren <jeff@unterbahn.com>
2012-01-22 01:39:16 -0500

    dragging/clicking and multitouch from Android/iOS

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ A half-baked idea to control the pointer of a Linux computer via a JavaScript we
 
 Requires:
 
-* Rails 3/Ruby 1.9.x
+* Rails 3
+* a Linux computer with a monitor and Gnome or something
 * xautomation ("sudo apt-get install xautomation")
 * a sense of adventure
 
-Keyboard input could also be achieved... as could a simple JavaScript VNC-style desktop view.
+To use:
+
+* boot it up in the fastest mode you can ("passenger start -e production -d" or "rails s -e production" or whatever)
+* navigate to the root "172.14.6.4:3000" and drag your mouse
+
+Try clicking your mouse. Keyboard input could also be achieved... as could a simple JavaScript VNC-style desktop view.
--- app/controllers/pointer_controller.rb
+++ app/controllers/pointer_controller.rb
@@ -1,15 +1,31 @@
 class PointerController < ApplicationController
 
   def index
-    
   end
 
   def mousemove
-    render :text => system("xte "+params[:x].to_i.to_s+","+params[:y].to_i.to_s)
+    cmd = "xte \"mousemove "+params[:x].to_i.to_s+" "+params[:y].to_i.to_s+"\""
+    render :text => system(cmd)
   end
 
   def mousermove
-    render :text => system("xte "+params[:x].to_i.to_s+","+params[:y].to_i.to_s)
+    cmd = "xte \"mousermove "+params[:x].to_i.to_s+" "+params[:y].to_i.to_s+"\""
+    render :text => system(cmd)
+  end
+
+  def click
+    cmd = "xte \"mouseclick 1\""
+    render :text => system(cmd)
+  end
+
+  def leftbtndown
+    cmd = "xte \"mousedown 1\""
+    render :text => system(cmd)
+  end
+
+  def leftbtnup
+    cmd = "xte \"mouseup 1\""
+    render :text => system(cmd)
   end
 
 end
